697114533b98d2bf2590899042fd2238e745d063
Mark <mark.dekin@gmail.com>
2024-10-07 17:44:23 -0400

    chore: reorganize and alphabetize env variables

--- start/env.ts
+++ start/env.ts
@@ -12,22 +12,28 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
+  /*
+  |----------------------------------------------------------
+  | Variables for base app configuration
+  |----------------------------------------------------------
+  */
   APP_KEY: Env.schema.string(),
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+  PORT: Env.schema.number(),
+  TZ: Env.schema.enum(['UTC'] as const),
 
   /*
   |----------------------------------------------------------
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
+  DB_DATABASE: Env.schema.string(),
   DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_PASSWORD: Env.schema.string.optional(),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string(),
 
   /*
   |----------------------------------------------------------
