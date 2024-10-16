ffa99164805b7751c206b244e48735ce5d4c28f9
Mark <mark.dekin@gmail.com>
2024-10-16 19:35:06 -0400

    feat: create user table migration

--- database/migrations/1728336746842_create_users_table.ts
+++ database/migrations/1728336746842_create_users_table.ts
@@ -1,14 +1,21 @@
 import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'users'
+  protected usersTable = 'users'
 
   async up() {
-    this.schema.createTable(this.tableName, (table) => {
+    this.schema.createTable(this.usersTable, (table) => {
       table.increments('id').notNullable()
-      table.string('full_name').nullable()
+
       table.string('email', 254).notNullable().unique()
+      table.string('first_name').notNullable()
+      table.boolean('is_admin').defaultTo(false).notNullable()
+      table.string('last_name').notNullable()
+      table.string('middle_name').nullable()
+      table.string('name_suffix').nullable()
       table.string('password').notNullable()
+      table.string('phone_extension').nullable()
+      table.string('phone_number').notNullable()
 
       table.timestamp('created_at').notNullable()
       table.timestamp('updated_at').nullable()
@@ -16,6 +23,6 @@ export default class extends BaseSchema {
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.usersTable)
   }
 }
