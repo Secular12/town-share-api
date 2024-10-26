6c1811a2ca3a83309c8e191c700554a3be1805ff
Mark <mark.dekin@gmail.com>
2024-10-26 15:01:58 -0400

    fix(factory): user factory still had phone
    this was removed from the model to later be added as a one-to-many

--- database/factories/user_factory.ts
+++ database/factories/user_factory.ts
@@ -15,11 +15,6 @@ export const UserFactory = factory
       middleName: FakerUtils.randomCall(faker.person.middleName, 0.2),
       nameSuffix: FakerUtils.randomCall(faker.person.suffix, 0.1),
       password: 'Change-Me-1',
-      phoneExtension: FakerUtils.randomCall(
-        () => faker.string.numeric({ length: { max: 4, min: 1 } }),
-        0.1
-      ),
-      phoneNumber: FakerUtils.phoneNumber(),
     }
   })
   .build()
